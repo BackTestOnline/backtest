04320011486ca8348ac394ebae7277a39c1ae3a1
jon <jon.james@backtestonline.com>
2019-06-14 22:51:47 +0100

    changing db credentials for live system

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
+$db_user = "backtest";
+$db_pass = "Dr4g0n5!";
 $db_name = "backtest_web";
 
 $connection = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
