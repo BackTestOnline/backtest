b53fca0ec6ae7131a5a64dbccd72f46ecb3022b5
jon <jon.james@backtestonline.com>
2019-06-17 20:21:07 +0100

    no message

---
+++ admin/create_user.php
@@ -0,0 +1,11 @@
+<?php
+include "includes/header.php";
+?>
+
+<h1>Add User:</h1>
+
+
+
+<?php
+include "includes/footer.php";
+?>
--- admin/includes/admin_side_links.php
+++ admin/includes/admin_side_links.php
@@ -2,8 +2,8 @@
 ?>
 <div class="vertical-menu">
     <a href="index.php">Admin Home</a>
-    <a href="add_post.php">Create Post</a>
+    <a href="create_post.php">Create Post</a>
     <a href="posts.php">Manage Posts</a>
-    <a href="add_user.php">Create User</a>
+    <a href="create_user.php">Create User</a>
     <a href="users.php">Manage Users</a>
 </div>
