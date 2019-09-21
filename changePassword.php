2b60bd767639f45779fbed09e7b6ad284573a46d
jon <jon.james@backtestonline.com>
2019-09-21 10:29:29 +0100

    added feature for users to change their passwords

---
+++ changePassword.php
@@ -0,0 +1,14 @@
+<?php
+include "includes/header.php";
+?>
+
+<div style="width:100%">
+    <div style="text-align: center; font-size: 4em"><span class="fa fa-user"></span></div>
+    <div style="font-size: 2em; text-align: center; font-weight: bold">Change Password</div>
+</div>
+
+
+
+<?php
+include "includes/footer.php";
+?>
